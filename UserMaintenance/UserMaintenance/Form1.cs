ef49cc595390588ed6030da5bded127d21567fa6
Martin Ádám Kimmel <martin.kimmel@stud.uni-corvinus.hu>
2020-09-22 11:44:06 +0200

    Added Useradding function

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,17 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
             label1.Text = Resource.LastName;
             label2.Text = Resource.FirstName;
             button1.Text = Resource.Add;
+            button1.Click += Button1_Click;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            User newuser = new User();
+            newuser.LastName = label1.Text;
+            newuser.FirstName = label2.Text;
+            users.Add(newuser);
         }
     }
 }
