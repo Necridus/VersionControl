c0fca3195c0b9f5f9ab3647f1363168c006d5e1a
Martin Ádám Kimmel <martin.kimmel@stud.uni-corvinus.hu>
2020-09-29 17:15:45 +0200

    Created methods to create table and created formatTable method

--- week4/week4/Form1.cs
+++ week4/week4/Form1.cs
@@ -18,6 +18,19 @@ namespace week4
 
         RealEstateEntities context = new RealEstateEntities();
 
+        string[] headers = new string[]
+{
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+};
+
         Excel.Application xlApp;
         Excel.Workbook xlWB;
         Excel.Worksheet xlSheet;
@@ -31,6 +44,7 @@ namespace week4
                 xlWB = xlApp.Workbooks.Add(Missing.Value);
                 xlSheet = xlWB.ActiveSheet;
                 CreateTable();
+                FormatTable();
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
             }
@@ -51,7 +65,73 @@ namespace week4
         }
         void CreateTable()
         {
+            object[,] values = new object[Flats.Count, headers.Length];
+            for (int col = 1; col <= headers.Length; col++)
+            {
+                xlSheet.Cells[1, col] = headers[col - 1];
+            }
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
 
+                string lift = "";
+                if (f.Elevator == true)
+                {
+                    lift = "Van";
+                }
+                else
+                {
+                    lift = "Nincs";
+                }
+                values[counter, 4] = lift;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = String.Format("={0}/{1}",GetCell(counter+2,7),GetCell(counter+2,8));
+                counter++;
+                xlSheet.get_Range(GetCell(2, 1), GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+            }
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+        void FormatTable()
+        {
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            int lastRowID = xlSheet.UsedRange.Rows.Count;
+            Excel.Range tableRange = xlSheet.get_Range(GetCell(2, 1), GetCell(lastRowID, headers.Length));
+            tableRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+            Excel.Range firstColRange = xlSheet.get_Range(GetCell(2, 1), GetCell(lastRowID, 1));
+            firstColRange.Font.Bold = true;
+            firstColRange.Interior.Color = Color.LightYellow;
+            Excel.Range lastColRange = xlSheet.get_Range(GetCell(1, headers.Length), GetCell(lastRowID, headers.Length));
+            lastColRange.Interior.Color = Color.LightGreen;
+            lastColRange.NumberFormat = "$????.??";
         }
     }
 }
