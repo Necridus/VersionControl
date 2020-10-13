75eb40f32efe18b1a43cebc61806cd9018579f4f
Martin Ádám Kimmel <martin.kimmel@stud.uni-corvinus.hu>
2020-10-13 16:18:24 +0200

    created dinamic Chart

--- week6/week6/Form1.Designer.cs
+++ week6/week6/Form1.Designer.cs
@@ -28,27 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.ratesDGW = new System.Windows.Forms.DataGridView();
+            this.chartRateData = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.startDateDP = new System.Windows.Forms.DateTimePicker();
+            this.endDateDP = new System.Windows.Forms.DateTimePicker();
+            this.currencyCB = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.ratesDGW)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).BeginInit();
             this.SuspendLayout();
             // 
             // ratesDGW
             // 
             this.ratesDGW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.ratesDGW.Location = new System.Drawing.Point(12, 12);
+            this.ratesDGW.Location = new System.Drawing.Point(12, 91);
             this.ratesDGW.Name = "ratesDGW";
-            this.ratesDGW.Size = new System.Drawing.Size(776, 426);
+            this.ratesDGW.Size = new System.Drawing.Size(327, 347);
             this.ratesDGW.TabIndex = 0;
+            // 
+            // chartRateData
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chartRateData.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chartRateData.Legends.Add(legend4);
+            this.chartRateData.Location = new System.Drawing.Point(345, 91);
+            this.chartRateData.Name = "chartRateData";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chartRateData.Series.Add(series4);
+            this.chartRateData.Size = new System.Drawing.Size(443, 347);
+            this.chartRateData.TabIndex = 1;
+            this.chartRateData.Text = "chart1";
+            // 
+            // startDateDP
+            // 
+            this.startDateDP.Location = new System.Drawing.Point(12, 30);
+            this.startDateDP.Name = "startDateDP";
+            this.startDateDP.Size = new System.Drawing.Size(200, 20);
+            this.startDateDP.TabIndex = 2;
+            this.startDateDP.ValueChanged += new System.EventHandler(this.startDateDP_ValueChanged);
+            // 
+            // endDateDP
+            // 
+            this.endDateDP.Location = new System.Drawing.Point(231, 30);
+            this.endDateDP.Name = "endDateDP";
+            this.endDateDP.Size = new System.Drawing.Size(200, 20);
+            this.endDateDP.TabIndex = 3;
+            this.endDateDP.ValueChanged += new System.EventHandler(this.endDateDP_ValueChanged);
+            // 
+            // currencyCB
+            // 
+            this.currencyCB.FormattingEnabled = true;
+            this.currencyCB.Items.AddRange(new object[] {
+            "EUR",
+            "USD"});
+            this.currencyCB.Location = new System.Drawing.Point(449, 30);
+            this.currencyCB.Name = "currencyCB";
+            this.currencyCB.Size = new System.Drawing.Size(121, 21);
+            this.currencyCB.TabIndex = 4;
+            this.currencyCB.Text = "EUR";
+            this.currencyCB.SelectedIndexChanged += new System.EventHandler(this.currencyCB_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.currencyCB);
+            this.Controls.Add(this.endDateDP);
+            this.Controls.Add(this.startDateDP);
+            this.Controls.Add(this.chartRateData);
             this.Controls.Add(this.ratesDGW);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.ratesDGW)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +114,10 @@
         #endregion
 
         private System.Windows.Forms.DataGridView ratesDGW;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRateData;
+        private System.Windows.Forms.DateTimePicker startDateDP;
+        private System.Windows.Forms.DateTimePicker endDateDP;
+        private System.Windows.Forms.ComboBox currencyCB;
     }
 }
 
--- week6/week6/Form1.cs
+++ week6/week6/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using week6.Entities;
 using week6.MnbServiceReference;
@@ -20,10 +21,17 @@ namespace week6
         public Form1()
         {
             InitializeComponent();
+            RefreshData();
+        }
 
+        private void RefreshData()
+        {
+            Rates.Clear();
             ratesDGW.DataSource = Rates;
+            chartRateData.DataSource = Rates;
             CallWebService();
             ProcessXML();
+            ShowData();
         }
 
         public void CallWebService()
@@ -31,9 +39,9 @@ namespace week6
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = currencyCB.SelectedItem.ToString(),
+                startDate = startDateDP.Value.ToString(),
+                endDate = endDateDP.Value.ToString()
             };
             var response = mnbService.GetExchangeRates(request);
             result = response.GetExchangeRatesResult;
@@ -51,12 +59,42 @@ namespace week6
                 rateData.Currency = childElement.GetAttribute("curr");
                 var unit = decimal.Parse(childElement.GetAttribute("unit"));
                 var value = decimal.Parse(childElement.InnerText);
-                if (unit!=0)
+                if (unit != 0)
                 {
                     rateData.Value = value / unit;
                 }
             }
         }
 
+        private void ShowData()
+        {
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void startDateDP_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void endDateDP_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void currencyCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
     }
 }
+
