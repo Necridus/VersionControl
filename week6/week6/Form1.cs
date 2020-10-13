c2ac0e49f5b73cbafc88cdb6669d430684e3a35b
Martin Ádám Kimmel <martin.kimmel@stud.uni-corvinus.hu>
2020-10-13 15:39:32 +0200

    created CallWebService function

--- week6/week6/Form1.cs
+++ week6/week6/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week6.MnbServiceReference;
 
 namespace week6
 {
@@ -15,6 +16,21 @@ namespace week6
         public Form1()
         {
             InitializeComponent();
+
+            CallWebService();
+        }
+
+        private void CallWebService()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
