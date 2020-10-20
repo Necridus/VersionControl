311f2b1975956bd636cc05ff39f44dec8fe6ee82
Martin Ádám Kimmel <martin.kimmel@stud.uni-corvinus.hu>
2020-10-20 21:16:35 +0200

    finished simulation steps

--- week7/week7/Form1.Designer.cs
+++ week7/week7/Form1.Designer.cs
@@ -28,20 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.endyearL = new System.Windows.Forms.Label();
+            this.propL = new System.Windows.Forms.Label();
+            this.fileTB = new System.Windows.Forms.TextBox();
+            this.browseBT = new System.Windows.Forms.Button();
+            this.startBT = new System.Windows.Forms.Button();
+            this.endyearNUD = new System.Windows.Forms.NumericUpDown();
+            this.resultsRTB = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.endyearNUD)).BeginInit();
             this.SuspendLayout();
+            // 
+            // endyearL
+            // 
+            this.endyearL.AutoSize = true;
+            this.endyearL.Location = new System.Drawing.Point(13, 13);
+            this.endyearL.Name = "endyearL";
+            this.endyearL.Size = new System.Drawing.Size(35, 13);
+            this.endyearL.TabIndex = 0;
+            this.endyearL.Text = "label1";
+            // 
+            // propL
+            // 
+            this.propL.AutoSize = true;
+            this.propL.Location = new System.Drawing.Point(287, 13);
+            this.propL.Name = "propL";
+            this.propL.Size = new System.Drawing.Size(35, 13);
+            this.propL.TabIndex = 1;
+            this.propL.Text = "label2";
+            // 
+            // fileTB
+            // 
+            this.fileTB.Location = new System.Drawing.Point(362, 10);
+            this.fileTB.Name = "fileTB";
+            this.fileTB.Size = new System.Drawing.Size(171, 20);
+            this.fileTB.TabIndex = 2;
+            // 
+            // browseBT
+            // 
+            this.browseBT.Location = new System.Drawing.Point(540, 10);
+            this.browseBT.Name = "browseBT";
+            this.browseBT.Size = new System.Drawing.Size(75, 23);
+            this.browseBT.TabIndex = 3;
+            this.browseBT.Text = "button1";
+            this.browseBT.UseVisualStyleBackColor = true;
+            this.browseBT.Click += new System.EventHandler(this.browseBT_Click);
+            // 
+            // startBT
+            // 
+            this.startBT.Location = new System.Drawing.Point(621, 10);
+            this.startBT.Name = "startBT";
+            this.startBT.Size = new System.Drawing.Size(75, 23);
+            this.startBT.TabIndex = 4;
+            this.startBT.Text = "button2";
+            this.startBT.UseVisualStyleBackColor = true;
+            this.startBT.Click += new System.EventHandler(this.startBT_Click);
+            // 
+            // endyearNUD
+            // 
+            this.endyearNUD.Location = new System.Drawing.Point(55, 10);
+            this.endyearNUD.Maximum = new decimal(new int[] {
+            2025,
+            0,
+            0,
+            0});
+            this.endyearNUD.Name = "endyearNUD";
+            this.endyearNUD.Size = new System.Drawing.Size(71, 20);
+            this.endyearNUD.TabIndex = 5;
+            // 
+            // resultsRTB
+            // 
+            this.resultsRTB.Location = new System.Drawing.Point(12, 36);
+            this.resultsRTB.Name = "resultsRTB";
+            this.resultsRTB.Size = new System.Drawing.Size(684, 442);
+            this.resultsRTB.TabIndex = 6;
+            this.resultsRTB.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(724, 490);
+            this.ClientSize = new System.Drawing.Size(710, 490);
+            this.Controls.Add(this.resultsRTB);
+            this.Controls.Add(this.endyearNUD);
+            this.Controls.Add(this.startBT);
+            this.Controls.Add(this.browseBT);
+            this.Controls.Add(this.fileTB);
+            this.Controls.Add(this.propL);
+            this.Controls.Add(this.endyearL);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.endyearNUD)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label endyearL;
+        private System.Windows.Forms.Label propL;
+        private System.Windows.Forms.TextBox fileTB;
+        private System.Windows.Forms.Button browseBT;
+        private System.Windows.Forms.Button startBT;
+        private System.Windows.Forms.NumericUpDown endyearNUD;
+        private System.Windows.Forms.RichTextBox resultsRTB;
     }
 }
 
--- week7/week7/Form1.cs
+++ week7/week7/Form1.cs
@@ -14,26 +14,39 @@ namespace week7
 {
     public partial class Form1 : Form
     {
+        #region Variables
         List<Person> Population = new List<Person>();
         List<BirthProbability> birthProbabilities = new List<BirthProbability>();
         List<DeathProbability> deathProbabilities = new List<DeathProbability>();
+        List<int> NumberOfMales = new List<int>();
+        List<int> NumberOfFemales = new List<int>();
         Random rnd = new Random(1234);
+        string popfile = "";
+        int startyear = 2005;
+        int endyear;
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
-            Population = GetPopulation(@"C:\Temp\nép.csv");
-            birthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
-            deathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
-            Simulation();
+            endyearL.Text = Resource1.Zaroev;
+            propL.Text = Resource1.Nepessegfajl;
+            fileTB.Text = popfile;
+            browseBT.Text = Resource1.Browse;
+            startBT.Text = Resource1.Start;
         }
-
         private void Simulation()
         {
-            for (int year = 2005; year <= 2024; year++)
+            NumberOfMales.Clear();
+            NumberOfFemales.Clear();
+            resultsRTB.Clear();
+            endyear = int.Parse(endyearNUD.Value.ToString());
+            for (int year = startyear; year <= endyear; year++)
             {
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    //majd
+                    Person person = Population[i];
+                    SimStep(year, person);
                 }
                 int nbrOfMales = (from x in Population
                                   where x.Gender == Gender.Male && x.IsAlive
@@ -41,7 +54,46 @@ namespace week7
                 int nbrOfFemales = (from x in Population
                                     where x.Gender == Gender.Female && x.IsAlive
                                     select x).Count();
-                Console.WriteLine(string.Format("Év: {0} Férfiak: {1} Nők: {2}", year, nbrOfMales, nbrOfFemales));
+                NumberOfMales.Add(nbrOfMales);
+                NumberOfFemales.Add(nbrOfFemales);
+            }
+        }
+
+        private void DisplayResults(int year, List<int> NumberOfMales, List<int> NumberOfFemales)
+        {
+            string newline = Environment.NewLine;
+            for (int i = 0; i < NumberOfFemales.Count(); i++)
+            {
+                resultsRTB.AppendText(String.Format("{1}Szimulációs év: {0}{1}\tFérfiak: {2}{1}\tNők: {3}{1}", year + i, newline, NumberOfMales[i], NumberOfFemales[i]));
+            }
+
+        }
+
+        public void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive)
+            {
+                return;
+            }
+            int age = year - person.BirthYear;
+            double deathprop = (from x in deathProbabilities
+                                where x.Gender == person.Gender && x.Age == age
+                                select x.Probability).FirstOrDefault();
+            if (rnd.NextDouble() < deathprop) person.IsAlive = false;
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double birthprop = (from x in birthProbabilities
+                                    where x.Age == age
+                                    select x.Probability).FirstOrDefault();
+                if (rnd.NextDouble() < birthprop)
+                {
+                    Person newborn = new Person();
+                    newborn.BirthYear = year;
+                    newborn.Gender = (Gender)(rnd.Next(1, 3));
+                    newborn.NbrOfChildren = 0;
+                    newborn.IsAlive = true;
+                    Population.Add(newborn);
+                }
             }
         }
 
@@ -86,20 +138,48 @@ namespace week7
         public List<Person> GetPopulation(string csvpath)
         {
             List<Person> population = new List<Person>();
-            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            if (csvpath != "")
             {
-                while (!sr.EndOfStream)
+                using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
                 {
-                    var line = sr.ReadLine().Split(';');
-                    population.Add(new Person()
+                    while (!sr.EndOfStream)
                     {
-                        BirthYear = int.Parse(line[0]),
-                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
-                        NbrOfChildren = int.Parse(line[2])
-                    });
+                        var line = sr.ReadLine().Split(';');
+                        population.Add(new Person()
+                        {
+                            BirthYear = int.Parse(line[0]),
+                            Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                            NbrOfChildren = int.Parse(line[2])
+                        });
+                    }
+                }
+                return population;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void startBT_Click(object sender, EventArgs e)
+        {
+            Simulation();
+            DisplayResults(startyear, NumberOfMales, NumberOfFemales);
+        }
+
+        private void browseBT_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    popfile = ofd.FileName;
+                    fileTB.Text = popfile;
+                    Population = GetPopulation(popfile);
+                    birthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
+                    deathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
                 }
             }
-            return population;
         }
     }
 }
