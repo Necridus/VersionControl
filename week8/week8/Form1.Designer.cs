a33af6ce22342d87a9e57aafa21e0a54cd96adac
Martin Ádám Kimmel <martin.kimmel@stud.uni-corvinus.hu>
2020-11-10 09:37:46 +0100

    edited form to be able to choose next toy

--- week8/week8/Form1.Designer.cs
+++ week8/week8/Form1.Designer.cs
@@ -32,6 +32,9 @@
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.nextLB = new System.Windows.Forms.Label();
+            this.carFactoryBT = new System.Windows.Forms.Button();
+            this.ballFactoryBT = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -55,15 +58,48 @@
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
+            // nextLB
+            // 
+            this.nextLB.AutoSize = true;
+            this.nextLB.Location = new System.Drawing.Point(253, 37);
+            this.nextLB.Name = "nextLB";
+            this.nextLB.Size = new System.Drawing.Size(70, 13);
+            this.nextLB.TabIndex = 1;
+            this.nextLB.Text = "Coming Next:";
+            // 
+            // carFactoryBT
+            // 
+            this.carFactoryBT.Location = new System.Drawing.Point(13, 32);
+            this.carFactoryBT.Name = "carFactoryBT";
+            this.carFactoryBT.Size = new System.Drawing.Size(75, 23);
+            this.carFactoryBT.TabIndex = 2;
+            this.carFactoryBT.Text = "CAR";
+            this.carFactoryBT.UseVisualStyleBackColor = true;
+            this.carFactoryBT.Click += new System.EventHandler(this.carFactoryBT_Click);
+            // 
+            // ballFactoryBT
+            // 
+            this.ballFactoryBT.Location = new System.Drawing.Point(118, 32);
+            this.ballFactoryBT.Name = "ballFactoryBT";
+            this.ballFactoryBT.Size = new System.Drawing.Size(75, 23);
+            this.ballFactoryBT.TabIndex = 3;
+            this.ballFactoryBT.Text = "BALL";
+            this.ballFactoryBT.UseVisualStyleBackColor = true;
+            this.ballFactoryBT.Click += new System.EventHandler(this.ballFactoryBT_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 450);
+            this.Controls.Add(this.ballFactoryBT);
+            this.Controls.Add(this.carFactoryBT);
+            this.Controls.Add(this.nextLB);
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,6 +108,9 @@
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Label nextLB;
+        private System.Windows.Forms.Button carFactoryBT;
+        private System.Windows.Forms.Button ballFactoryBT;
     }
 }
 
--- week8/week8/Form1.cs
+++ week8/week8/Form1.cs
@@ -7,51 +7,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week8.Abstractions;
 using week8.Entities;
 
 namespace week8
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
-        private BallFactory _factory;
-        public BallFactory Factory
+        private List<Toy> _toys = new List<Toy>();
+        private IToyFactory _factory;
+        private Toy _nextToy;
+        public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set 
+            {
+                _factory = value;
+                DisplayNext();
+            }
         }
 
         public Form1()
         {
             InitializeComponent();
-            Factory = new BallFactory();
         }
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            var ball = Factory.CreateNew();
-            ball.Left = -ball.Width;
-            _balls.Add(ball);
-            mainPanel.Controls.Add(ball);
+            var toy = Factory.CreateNew();
+            toy.Left = -toy.Width;
+            _toys.Add(toy);
+            mainPanel.Controls.Add(toy);
         }
 
+        public void DisplayNext()
+        {
+            if (_nextToy != null)
+            {
+                Controls.Remove(_nextToy);
+            }
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = nextLB.Top + nextLB.Height + 20;
+        }
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             int maxposition = 0;
-            foreach (var ball in _balls)
+            foreach (var toy in _toys)
             {
-                ball.MoveBall();
-                if (ball.Left>maxposition)
+                toy.MoveToy();
+                if (toy.Left>maxposition)
                 {
-                    maxposition = ball.Left;
+                    maxposition = toy.Left;
                 }
             }
             if (maxposition>=1000)
             {
-                var firstBall = _balls[0];
-                mainPanel.Controls.Remove(firstBall);
-                _balls.Remove(firstBall);
+                var firsttoy = _toys[0];
+                mainPanel.Controls.Remove(firsttoy);
+                _toys.Remove(firsttoy);
             }
+        }
+
+        private void carFactoryBT_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void ballFactoryBT_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
         }
     }
 }
