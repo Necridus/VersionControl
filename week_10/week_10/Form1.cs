5d8f2d6a07d06cff0cac11216b1ce8ae2eb9c014
Martin Ádám Kimmel <martin.kimmel@stud.uni-corvinus.hu>
2020-11-24 11:15:32 +0100

    created and displayed GameController and GameArea elements on Form1

--- week_10/week_10/Form1.cs
+++ week_10/week_10/Form1.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace week_10
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga = new GameArea();
         public Form1()
         {
             InitializeComponent();
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
         }
     }
 }
